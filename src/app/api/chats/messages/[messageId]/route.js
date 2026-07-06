b50608be1603ee11ae8063bd8517fa214cacddc3
authur4q <authurbass@gmail.com>
2026-07-07 01:26:17 +0300

    sec check

--- src/app/api/chats/messages/[messageId]/route.js
+++ src/app/api/chats/messages/[messageId]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import Message from '../../../../../models/messages';
-import connectMongoDb from '../../../../../lib/mongodb';
+import Message from '../../../../../../models/messages';
+import connectMongoDb from "../../../../../../lib/mongodb"
 
 
 export async function DELETE(request, { params }) {
